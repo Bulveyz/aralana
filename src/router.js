03b36f0ec23c6de60f60ee22a56ac38027a5646a
Ruslan Ibragimov <rus131354@icloud.com>
2019-04-22 19:02:20 +0600

    add path

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Aralan from "./views/Aralan";
 
 Vue.use(Router)
 
@@ -12,6 +13,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/arlan',
+      component: Aralan
     },
     {
       path: '/about',
---
+++ src/views/Aralan.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Aralon</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Aralan"
+    }
+</script>
+
+<style scoped>
+
+</style>
