fee8e4838a48ca3317a24ae010494a984c1828a6
Pixel <arlan.tuak@gmail.com>
2019-04-23 16:20:44 +0600

    menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <v-toolbar-title color="white">Doctor Pointer</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
-            <v-menu offset-y class="mr-3" :nudge-width="100">
+            <v-menu left offset-y class="mr-3" :nudge-width="100">
                 <template slot='activator'>
                     <v-toolbar-title>
                         <v-badge class="d-flex custom-badge" left color="pink accent-3">
@@ -16,7 +16,14 @@
                 </template>
 
                 <v-list>
-
+                    <template v-for="(record, index) in record">
+                        <v-list-tile>
+                            <v-list-tile-content>
+                                <v-list-tile-title v-html="record.title"></v-list-tile-title>
+                                <v-list-tile-sub-title v-html="record.subtitle"></v-list-tile-sub-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </template>
                 </v-list>
             </v-menu>
             <v-btn flat>Мои записи</v-btn>
@@ -29,7 +36,21 @@
         name: "Navbar",
         data() {
             return {
-                notification: false
+                notification: false,
+                record: [
+                    {
+                        title: 'Запись',
+                        subtitle: "<span class='text--primary'>Сегодня в 09:30</span> &mdash; запись к эндокринологу"
+                    },
+                    {
+                        title: 'Запись',
+                        subtitle: "<span class='text--primary'>Ваша заявка одобрена</span> &mdash; завтра в 09:30 запись к эндокринологу"
+                    },
+                    {
+                        title: 'Запись',
+                        subtitle: "<span class='text--primary'>Ваша заявка принята</span> &mdash; запись к эндокринологу"
+                    }
+                ]
             }
         }
     }
