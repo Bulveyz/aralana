82887815491f7873a3379e66cc0c92b04db2a6e4
Pixel <arlan.tuak@gmail.com>
2019-04-24 00:19:58 +0600

    add badge

--- src/views/MyAppointment.vue
+++ src/views/MyAppointment.vue
@@ -1,15 +1,49 @@
 <template>
     <v-container>
-       
         <v-list three-line class="custom-doctor-list">
-                <template v-for="(records, index) in records">
+                <template v-for="(record, index) in records">
                     <v-list-tile class="mb-4 elevation-1" :key="index">
+                        <v-list-tile-avatar class="badge-indicator">
+                                <v-badge v-if="record.status == 'cancle'">
+                                    <template v-slot:badge>
+                                        <span class="text-primary">.</span>
+                                    </template>
+                                </v-badge>
+                               <v-badge color="green accent-3" v-else-if="record.status == 'active'">
+                                    <template v-slot:badge>
+                                        <span class="text-green">.</span>
+                                    </template>
+                                </v-badge>
+                                <v-badge color="yellow" v-else-if="record.status == 'await'">
+                                    <template v-slot:badge>
+                                        <span class="text-yellow">.</span>
+                                    </template>
+                                </v-badge>
+                        </v-list-tile-avatar>
                         <v-list-tile-content>
-                            <v-list-tile-title v-html="records.title"></v-list-tile-title>
-                            <v-list-tile-sub-title v-html="records.subtitle"></v-list-tile-sub-title>
+                            <v-list-tile-title v-html="record.title"></v-list-tile-title>
+                            
+                            <v-list-tile-sub-title v-html="record.subtitle"></v-list-tile-sub-title>
+                          
                         </v-list-tile-content>
-                        <v-btn v-if="records.active" color="warning">Отменить запись</v-btn>
-                        <v-btn color="info">Подробнее</v-btn>
+                        
+                        <v-btn v-if="record.active" color="primary" flat>Отменить запись</v-btn>
+                        <v-btn color="primary">Подробнее</v-btn>
+                    </v-list-tile>
+                </template>
+                <template v-for="(history, index) in historys">
+                    <v-list-tile class="mb-4 elevation-1" :key="index">
+                        <v-list-tile-avatar class="badge-indicator">
+                        <v-badge color="blue">
+                                    <template v-slot:badge>
+                                        <span class="text-primary">.</span>
+                                    </template>
+                                </v-badge>
+                         </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title v-html="history.title"></v-list-tile-title>
+                            <v-list-tile-sub-title v-html="history.subtitle"></v-list-tile-sub-title>  
+                        </v-list-tile-content>
                     </v-list-tile>
                 </template>
         </v-list>
@@ -32,33 +66,42 @@
                     {
                         title: 'Запись',
                         active: true,
+                        status: 'active',
                         subtitle: "<span class='text--primary'>Сегодня в 09:30</span> &mdash; запись к эндокринологу"
                     },
                     {
-                        title: 'Запись одобрена',
+                        title: 'Запись в обработке',
                         active: true,
-                        subtitle: "<span class='text--primary'>Ваша заявка одобрена</span> &mdash; завтра в 09:30 запись к эндокринологу"
+                        status: 'await',
+                        subtitle: "<span class='text--primary'>Ваша заявка в обработке</span> &mdash; запись к артопеду"
                     },
                     {
-                        title: 'Заявка принята',
+                        title: 'Заявка в обработке',
                         active: false,
-                        subtitle: "<span class='text--primary'>Ваша заявка принята</span> &mdash; запись к эндокринологу"
+                        status: 'await',
+                        subtitle: "<span class='text--primary'>Ваша заявка в обработке</span> &mdash; запись к отоларингологу"
+                    }
+                    
+                ],
+                historys: [
+                    {
+                        title: 'Запись',
+                        active: false,
+                        status: '',
+                        subtitle: "<span class='text--primary'>Запись</span> &mdash; запись к окулисту"
                     },
                     {
                         title: 'Запись',
                         active: false,
-                        subtitle: "<span class='text--primary'>Сегодня в 11:10</span> &mdash; запись к окулисту"
-                    },
-                    {
-                        title: 'Запись одобрена',
-                        active: false,
-                        subtitle: "<span class='text--primary'>Ваша заявка одобрена</span> &mdash; 11/06/19 в 11:10 запись к окулисту"
+                        status: '',
+                        subtitle: "<span class='text--primary'>Запись</span> &mdash; запись к неврологу"
                     },
                     {
                         title: 'Заявка принята',
                         active: false,
-                        subtitle: "<span class='text--primary'>Ваша заявка принята</span> &mdash; запись к окулисту"
-                    },
+                        status: '',
+                        subtitle: "<span class='text--primary'>Запись</span> &mdash; запись к педиатору"
+                    }
                 ]
             }
         }
